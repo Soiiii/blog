eb60e45e0357c6f3594df895bc3b9b9ed54d1878
Soiiii <cojomkr33@gmail.com>
2023-04-24 06:54:17 +0900

    메타코딩

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,4 +1,5 @@
 let index = {
+
     init: function(){
     // .on(1,2) 1번이 되면 2번에 무엇을 할건지 적는거(1:클릭, 2:)
         $("#btn-save").on("click", ()=>{
@@ -13,11 +14,30 @@ let index = {
             username: $("#username").val(),
             password: $("#password").val(),
             email: $("#email").val()
-        }
+        };
         //ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
-        $.ajax().done().fail();
+        //ajax 호출시 기본이 비동기 호출
+        //ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환해줌
+        $.ajax({
+            type:"POST",
+            url:"/blog/api/user",
+            data:JSON.stringify(data), // http body데이터
+            contentType:"application/json; charset=utf-8",
+             //요청을 서버로해서 응답이 왔을때 기본적으로 모든 것이 문자열 (생긴게 json이라면)
+             // => javascript 오브젝트로 변경
+            dataType:"json"
+
+        }).done(function(resp){
+            alert("회원가입 완료");
+            console.log(resp);
+            alert(resp);
+//            location.href="/blog";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+        }
 
     }
-}
+
 
 index.init();
