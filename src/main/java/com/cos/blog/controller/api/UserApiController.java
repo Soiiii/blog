45a642f3f2de7c26940db7563d11a5c42a4cce25
Soiiii <cojomkr33@gmail.com>
2023-04-25 07:11:01 +0900

    메타코딩

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.cos.blog.controller.api;
 
 import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,12 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user){
+        // User 클래스를 사용하면 username, password, email 들어가서
+        user.setRole(RoleType.USER);
         System.out.println("UserAPIController: Call Save");
-        //retrun 1을 하면
-
-        return new ResponseDto<Integer>(HttpStatus.OK,1);
+        userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
-
 }
--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.cos.blog.handler;
 
+import com.cos.blog.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,8 @@ public class GlobalExceptionHandler {
 
     //IllegalArgumentException 해당 에러가 나게되면 exception에 대한 에러를 e 함수에 전달을 해준다
     @ExceptionHandler(value=IllegalArgumentException.class)
-    public String handleArgumentException(IllegalArgumentException e){
-        return "<h1>" + e.getMessage() + "</h1>";
+    public ResponseDto<String> handleArgumentException(IllegalArgumentException e){
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
 
     // 해당되는 모든 Exception이 여기로 들어옴 (모든 exception의 부모 클래스)
