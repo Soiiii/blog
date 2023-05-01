d94e7bb7d2ed5500ec52ac928330f72e932971d5
Soiiii <cojomkr33@gmail.com>
2023-05-02 06:54:44 +0900

    메타코딩

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -4,7 +4,6 @@ import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +18,8 @@ public class UserApiController {
 
     //세션 객체를 스프링 컨트롤러가 빈으로 등록해서 가지고 있다(필요시 DI에서 불러와서 사용이 가능)
     //autowired를 해놓으면 파라미터로 session을 안 받아내도 됨
-    @Autowired
-    private HttpSession session;
+//    @Autowired
+//    private HttpSession session;
 
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user){
@@ -31,16 +30,16 @@ public class UserApiController {
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
 
-    @PostMapping("/api/user/login")
-    //HttpSession -> 세션을 만들어주는
+//    @PostMapping("/api/user/login")
+//    //HttpSession -> 세션을 만들어주는
 //    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
-    public ResponseDto<Integer> login(@RequestBody User user){
-        System.out.println("UserAPIController: Call Login");
-        User principal = userService.로그인(user); //principal = 접근주체라는 의미
-        if(principal != null){
-            //session set해주면 됨
-            session.setAttribute("principal", principal);
-        }
-        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-    }
+////    public ResponseDto<Integer> login(@RequestBody User user){
+//        System.out.println("UserAPIController: Call Login");
+//        User principal = userService.로그인(user); //principal = 접근주체라는 의미
+//        if(principal != null){
+//            //session set해주면 됨
+//            session.setAttribute("principal", principal);
+//        }
+//        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+//    }
 }
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<sec:authorize access="isAuthenticated()">
+    <sec:authentication property="principal" var="principal"/>
+</sec:authorize>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +17,7 @@
 
 </head>
 <body>
-
+<h1>principal</h1>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -26,7 +30,7 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
     <c:choose>
-        <c:when test="${empty sessionScope.principal}">
+        <c:when test="${empty principal}">
               <ul class="nav navbar-nav">
                 <a href="/loginForm">로그인</a></li>
                 <li><a href="/joinForm">회원가입</a></li>
